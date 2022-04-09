1287bed191203da3e65b905edd97cc08bb850050
连边 <ilianbian@163.com>
2022-04-09 22:39:05 +0800

    仓库初始化

---
+++ src/main/java/RedisStream/config/RedisStreamConfig.java
@@ -0,0 +1,9 @@
+package RedisStream.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class RedisStreamConfig {
+}
